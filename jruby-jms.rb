1173fa65ec4c8abb991ab9a8f018dd9207bc617a
nolim1t <nolimit@perceptionz.net>
2009-05-28 16:42:16 +1000

    Added a later copy of the JRUBY JMS Library (Most recent)

--- jruby-jms.rb
+++ jruby-jms.rb
@@ -1,5 +1,9 @@
-# JRUBY Queue Management 
-# This creates an entry onto a JMS Queue
+# JRUBY JMS Utilities 
+# This is a simple library which allows you to queue msgs, consume msgs from the queue, or count the message.
+
+# Libraries required. (connector-api-1.5.jar may require downloading. the rest are with the openMQ package)
+#export CLASSPATH="jms.jar:imq.jar:imqjmsra.jar:connector-api-1.5.jar"
+
 
 require "java"
 
@@ -8,27 +12,82 @@ include_class "com.sun.messaging.ConnectionFactory"
 include_class "com.sun.messaging.Queue"
 
 module JMS
-	class Producer
-		def initialize(queuename='JMSQUEUENAME', server='jmshost', port='7676', uid='admin', pwd='admin')
-			myConnFactory = ConnectionFactory.new
-			myConnFactory.setProperty("imqBrokerHostName", server)
-			myConnFactory.setProperty("imqBrokerHostPort", port)
-			myConnFactory.setProperty("imqDefaultUsername", uid)
-			myConnFactory.setProperty("imqDefaultPassword", pwd)
-
-			@myConn = myConnFactory.createConnection()
-			@mySess = @myConn.createSession(false, Session::AUTO_ACKNOWLEDGE)
-			@myQueue = Queue.new(queuename)
+	# Class name: QueueManager
+	# Constructor parameters (queuename, server, port, username, password)
+	# Instance Methods:
+	# Produce(msg) returns SUCCESS if successful
+	# Consume() returns MSG=<MSG> if successful
+	# Count() returns the length of the queue if successful
+	class QueueManager
+		def initialize(queuename='QUEUENAME', server='ADDR', port='7676', uid='admin', pwd='admin')
+			begin
+				myConnFactory = ConnectionFactory.new
+				myConnFactory.setProperty("imqBrokerHostName", server)
+				myConnFactory.setProperty("imqBrokerHostPort", port)
+				myConnFactory.setProperty("imqDefaultUsername", uid)
+				myConnFactory.setProperty("imqDefaultPassword", pwd)
+				@myConn = myConnFactory.createConnection
+				@mySess = @myConn.createSession(false, Session::AUTO_ACKNOWLEDGE)
+				@myQueue = Queue.new(queuename)
+				@connectionSuccess = "TRUE"
+			rescue
+				@connectionSuccess = "FALSE"
+			end
 		end
 		
 		def Produce(msg)
-			myMsgProducer = @mySess.createProducer(@myQueue)
-			myTextMsg = @mySess.createTextMessage()
-			myTextMsg.setText(msg)
-			myMsgProducer.send(myTextMsg)
+			begin
+				if @connectionSuccess == "TRUE"
+					myMsgProducer = @mySess.createProducer(@myQueue)
+					myTextMsg = @mySess.createTextMessage
+					myTextMsg.setText(msg)
+					myMsgProducer.send(myTextMsg)
 			
-			@mySess.close()
-			@myConn.close()			
+					@mySess.close
+					@myConn.close
+					return "SUCCESS"
+				else
+					return "ERR_NOCONNECTION" # There is no connection
+				end
+			rescue
+				return "ERR_FAILURE" # Something else broke
+			end
+		end
+		
+		def Consume
+			begin
+				myConsumer = @mySess.createConsumer(@myQueue)
+				@myConn.start()
+				receiverobj = myConsumer.receive(1000)
+				msg = ""
+				receiverobj.methods.each { |rm| if rm == "text" then; msg=receiverobj.text; end }
+				@mySess.close
+				@myConn.close			
+				if (msg.length > 1) then
+					return "#{msg}"
+				else
+					return "ERR_NOMESSAGES"
+				end
+			rescue
+				return "ERR_FAILURE"
+			end
+		end
+		
+		def Count
+			begin
+				browser = @mySess.createBrowser(@myQueue)
+				benum = browser.getEnumeration()
+				queue = 0
+				while benum.hasMoreElements() do
+					queue = queue + 1
+					benum.nextElement()
+				end
+				@mySess.close
+				@myConn.close			
+				return "#{queue}"
+			rescue
+				return "ERR_FAILURE"
+			end		
 		end
 	end
 end
