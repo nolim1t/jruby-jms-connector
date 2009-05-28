976f5ee469ccc04ee08048addbe49447ba3b7316
nolim1t <nolimit@perceptionz.net>
2009-05-28 16:34:35 +1000

    This is the first commit

---
+++ jruby-jms.rb
@@ -0,0 +1,37 @@
+# JRUBY Queue Management 
+# This creates an entry onto a JMS Queue
+
+# To create a new queue entry called TEST
+# jruby -e "require 'class.jruby-jms.rb';c=JMS::Producer.new('MM.C360.P5');.c.Produce('TEST');"
+
+require "java"
+
+include_class "javax.jms.Session"
+include_class "com.sun.messaging.ConnectionFactory"
+include_class "com.sun.messaging.Queue"
+
+module JMS
+	class Producer
+		def initialize(queuename='JMSQUEUENAME', server='jmshost', port='7676', uid='admin', pwd='admin')
+			myConnFactory = ConnectionFactory.new
+			myConnFactory.setProperty("imqBrokerHostName", server)
+			myConnFactory.setProperty("imqBrokerHostPort", port)
+			myConnFactory.setProperty("imqDefaultUsername", uid)
+			myConnFactory.setProperty("imqDefaultPassword", pwd)
+
+			@myConn = myConnFactory.createConnection()
+			@mySess = @myConn.createSession(false, Session::AUTO_ACKNOWLEDGE)
+			@myQueue = Queue.new(queuename)
+		end
+		
+		def Produce(msg)
+			myMsgProducer = @mySess.createProducer(@myQueue)
+			myTextMsg = @mySess.createTextMessage()
+			myTextMsg.setText(msg)
+			myMsgProducer.send(myTextMsg)
+			
+			@mySess.close()
+			@myConn.close()			
+		end
+	end
+end
